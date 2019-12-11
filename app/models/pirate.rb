acf2c4039352d6630cafb1218855328928c094bd
dut-a <d.dut@live.com>
2019-12-11 17:22:43 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,23 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(n, wt, ht)
+        @name = n
+        @weight = wt
+        @height = ht
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        self.all.clear
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(n, t, b)
+        @name = n
+        @type = t
+        @booty = b
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        self.all.clear
+    end
+
 end
